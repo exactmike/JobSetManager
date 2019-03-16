82b9763bfb9636b942889029183545228ea85e01
Mike Campbell <mike@campbellnetwork.net>
2019-03-16 11:25:26 -0400

    Add JobTypes.json file and import

--- JobSetManager.psm1
+++ JobSetManager.psm1
@@ -3,3 +3,4 @@
 ##########################################################################################################
 $FunctionFiles = Get-ChildItem -Recurse -File -Path $(Join-Path -Path $PSScriptRoot -ChildPath 'Functions')
 foreach ($ff in $FunctionFiles) {. $ff.fullname}
+$Script:JobTypes = Import-JSON -Path $(Join-Path -Path $PSScriptRoot -ChildPath 'JobTypes.json')
