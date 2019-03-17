21dad6a37f48ca6c85749909483b8141cab60fe4
Mike Campbell <mike@exactsolutions.biz>
2019-03-16 23:25:09 -0400

    Update Get-JSMJobCurrent to support multiple Job Engine/Framework types

--- Functions/Get-JSMJobCurrent.ps1
+++ Functions/Get-JSMJobCurrent.ps1
@@ -8,8 +8,20 @@ function Get-JSMJobCurrent
         [parameter(Mandatory)]
         [hashtable]$JobCompletion
     )
-    $CurrentRSJobs = @(Get-RSJob | Where-Object -FilterScript {$_.Name -in $JobRequired.Name -and $_.Name -notin $JobCompletion.Keys})
     $CurrentJobs = @{}
-    $CurrentRSJobs | Select-Object -ExpandProperty Name | Sort-Object -Unique | ForEach-Object {$CurrentJobs.$($_) =  $true}
+    $JobsPossiblyCurrent = $JobRequired | Where-Object -FilterScript {$_.Name -notin $JobCompletion.Keys}
+    $CurrentJFJobs = @(
+        foreach ($jpc in $JobsPossiblyCurrent)
+        {
+            $JobType = switch ($jpc.JobType) {{[string]::IsNullOrWhiteSpace($_)} {'RSJob'} default {$_}} #default to RSJob since this module first assumed this
+            $GetJobCommand = $script:JobTypes.where({$_.Name -eq $JobType}).commands.where({$_.Type -eq 'GetJob'}).Name
+            $GetJobParams = @{
+                ErrorAction = 'SilentlyContinue'
+                Name = $jpc.Name
+            }
+            &$GetJobCommand @GetJobParams
+        }
+    )
+    $CurrentJFJobs | Select-Object -ExpandProperty Name | Sort-Object -Unique | ForEach-Object {$CurrentJobs.$($_) =  $true}
     $CurrentJobs
 }
