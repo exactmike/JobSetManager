af6b5051af1dd387d391ac1f78a62fb59e8b6c70
exactmike <mike@exactsolutions.biz>
2019-04-19 21:50:55 -0400

    more improvements to Test-JSMJobResult

--- Functions/Test-JSMJobResult.ps1
+++ Functions/Test-JSMJobResult.ps1
@@ -26,14 +26,14 @@ function Test-JSMJobResult
             {
                 'AllowNull'
                 {
-                    $message = "$JobName : Processing Validation $_ ($($ResultsValidation.$_))"
+                    $message = "$JobName : Processing Validation AllowNull ($($ResultsValidation.AllowNull))"
                     Write-Verbose -Message $message
                     $Result = $null -eq $JobResults
                     if ($Result -eq $true)
                     {
-                        $message = "$JobName : Passed Validation $_ ($($ResultsValidation.$_))"
+                        $message = "$JobName : Passed Validation AllowNull ($($ResultsValidation.AllowNull))"
                         Write-Verbose -Message $message
-                        $message = "$JobName : Validation $_ ($($ResultsValidation.$_)) Passed. Skipping other validations."
+                        $message = "$JobName : Validation AllowNull ($($ResultsValidation.AllowNull)) Passed. Skipping other validations."
                         Write-Verbose -Message $message
                         $Result
                         break
@@ -41,14 +41,14 @@ function Test-JSMJobResult
                 }
                 'AllowEmptyArray'
                 {
-                    $message = "$JobName : Processing Validation $_ ($($ResultsValidation.$_))"
+                    $message = "$JobName : Processing Validation AllowEmptyArray ($($ResultsValidation.AllowEmptyArray))"
                     Write-Verbose -Message $message
                     $Result = $JobResults.count -eq 0
                     if ($Result -eq $true)
                     {
-                        $message = "$JobName : Passed Validation $_ ($($ResultsValidation.$_))"
+                        $message = "$JobName : Passed Validation $AllowEmptyArray ($($ResultsValidation.AllowEmptyArray))"
                         Write-Verbose -Message $message
-                        $message = "$JobName : Validation $_ ($($ResultsValidation.$_)) Passed. Skipping other validations."
+                        $message = "$JobName : Validation AllowEmptyArray ($($ResultsValidation.AllowEmptyArray)) Passed. Skipping other validations."
                         Write-Verbose -Message $message
                         $Result
                         break
@@ -56,68 +56,70 @@ function Test-JSMJobResult
                 }
                 'NotNull'
                 {
-                    $message = "$JobName : Processing Validation $_ ($($ResultsValidation.$_))"
+                    $message = "$JobName : Processing Validation NotNull ($($ResultsValidation.NotNull))"
                     Write-Verbose -Message $message
                     $Result = $null -ne $JobResults
-                    if ($Result -eq $true)
+                    switch ($Result)
                     {
-                        $message = "$JobName : Passed Validation $_ ($($ResultsValidation.$_))"
-                        Write-Verbose -Message $message
-                        $Result
+                        $true
+                        {
+                            $message = "$JobName : Passed Validation NotNull ($($ResultsValidation.NotNull))"
+                            Write-Verbose -Message $message
+                            $Result
+                        }
+                        $false
+                        {
+                            $message = "$JobName : FAILED Validation NotNull ($($ResultsValidation.NotNull)). Skipping other validations."
+                            Write-Warning -Message $message
+                            $Result
+                            break
+                        }
                     }
-                    else
-                    {
-                        $message = "$JobName : FAILED Validation $_ ($($ResultsValidation.$_)). Skipping other validations."
-                        Write-Warning -Message $message
-                        $Result
-                        break
-                    }
-
                 }
                 'ValidateType'
                 {
-                    $message = "$JobName : Processing Validation $_ ($($ResultsValidation.$_))"
+                    $message = "$JobName : Processing Validation ValidateType ($($ResultsValidation.ValidateType.Name))"
                     Write-Verbose -Message $message
-                    $Result = $JobResults -is $ResultsValidation.$_
+                    $Result = $JobResults -is $($ResultsValidation.ValidateType)
                     if ($Result -eq $true)
                     {
-                        $message = "$JobName : Passed Validation $_ ($($ResultsValidation.$_))"
+                        $message = "$JobName : Passed Validation ValidateType ($($ResultsValidation.ValidateType.Name))"
                         Write-Verbose -Message $message
                     }
                     if ($Result -eq $false)
                     {
-                        $message = "$JobName : Failed Validation $_ ($($ResultsValidation.$_))"
+                        $message = "$JobName : Failed Validation ValidateType ($($ResultsValidation.ValidateType.Name))"
                         Write-Warning -Message $message
                     }
                     $Result
                 }
                 'ValidateElementCountExpression'
                 {
-                    $message = "$JobName : Validation $_ ($($ResultsValidation.$_))"
+                    $message = "$JobName : Validation ValidateElementCountExpression ($($ResultsValidation.ValidateElementCountExpression))"
                     Write-Verbose -Message $message
-                    $Result = Invoke-Expression "$($JobResults.count) $($ResultsValidation.$_)"
+                    $Result = Invoke-Expression "$($JobResults.count) $($ResultsValidation.ValidateElementCountExpression)"
                     if ($Result -eq $true)
                     {
-                        $message = "$JobName : Validation $_ ($($ResultsValidation.$_)). Result Count: $($JobResults.count)"
+                        $message = "$JobName : Validation ValidateElementCountExpression ($($ResultsValidation.ValidateElementCountExpression)). Result Count: $($JobResults.count)"
                         Write-Verbose -Message $message
                     }
                     if ($Result -eq $false)
                     {
-                        $message = "$JobName : Validation $_ ($($ResultsValidation.$_)). Result Count: $($JobResults.count)"
+                        $message = "$JobName : Validation ValidateElementCountExpression ($($ResultsValidation.ValidateElementCountExpression)). Result Count: $($JobResults.count)"
                         Write-Warning -Message $message
                     }
                     $Result
                 }
                 'ValidateElementMember'
                 {
-                    $message = "$JobName : Validation $_ ($($ResultsValidation.$_))"
+                    $message = "$JobName : Validation ValidateElementMember ($($ResultsValidation.ValidateElementMember))"
                     Write-Verbose -Message $message
                     $Result = $(
                         $MemberNames = @($JobResults[0] | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name)
                         if
                         (
                             @(
-                                switch ($ResultsValidation.$_)
+                                switch ($ResultsValidation.ValidateElementMember)
                                 {
                                     {$_ -in $MemberNames}
                                     {$true}
@@ -132,29 +134,29 @@ function Test-JSMJobResult
                     )
                     if ($Result -eq $true)
                     {
-                        $message = "$JobName : Validation $_ ($($ResultsValidation.$_))"
+                        $message = "$JobName : Validation ValidateElementMember ($($ResultsValidation.ValidateElementMember))"
                         Write-Verbose -Message $message
                     }
                     if ($Result -eq $false)
                     {
-                        $message = "$JobName : Validation $_ ($($ResultsValidation.$_))"
+                        $message = "$JobName : Validation ValidateElementMember ($($ResultsValidation.ValidateElementMember))"
                         Write-Warning -Message $message
                     }
                     $Result
                 }
                 'ValidatePath'
                 {
-                    $message = "$JobName : Validation $_ ($($ResultsValidation.$_))"
+                    $message = "$JobName : Validation ValidatePath ($($ResultsValidation.ValidatePath))"
                     Write-Verbose -Message $message
                     $Result = Test-Path -path $JobResults
                     if ($Result -eq $true)
                     {
-                        $message = "$JobName : Validation $_ ($($ResultsValidation.$_))"
+                        $message = "$JobName : Validation ValidatePath ($($ResultsValidation.ValidatePath))"
                         Write-Verbose -Message $message
                     }
                     if ($Result -eq $false)
                     {
-                        $message = "$JobName : Validation $_ ($($ResultsValidation.$_))"
+                        $message = "$JobName : Validation ValidatePath ($($ResultsValidation.ValidatePath))"
                         Write-Warning -Message $message
                     }
                     $Result
